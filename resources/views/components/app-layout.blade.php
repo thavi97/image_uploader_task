d83385ba114d168b9508e6df5ac537d64ed6e9ea
Thavi <thavit97@gmail.com>
2026-08-14 11:49:53 +0100

    Get the index page working

---
+++ resources/views/components/app-layout.blade.php
@@ -0,0 +1,18 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image Uploader Task</title>
+</head>
+<body>
+
+    <!-- Page Content -->
+    <main>
+        <div>
+            {{ $slot }}
+        </div>
+    </main>
+
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <section>
+        <h1>Image Uploader</h1>
+
+        <p>testing</p>
+    </section>
+</x-app-layout>
