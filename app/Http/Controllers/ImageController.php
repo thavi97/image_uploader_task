e9e426099d05eb594f2f65b2f9d4ccacca6afcfe
Thavi <thavit97@gmail.com>
2026-08-14 12:19:25 +0100

    Resize oversized images to 1024x1024

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
 
 class ImageController extends Controller
 {
@@ -14,11 +18,18 @@ class ImageController extends Controller
                 'image',
                 'mimes:jpg,jpeg,png',
                 'max:2048', // 2MB
-                'dimensions:max_width=1024,max_height=1024',
+                'dimensions:max_width=5000,max_height=5000',
             ],
         ]);
 
-        $path = $request->file('image')->store('images', 'public');
+        $file = $request->file('image');
+
+        $manager = new ImageManager(new Driver());
+        $image = $manager->read($file)->scaleDown(1024, 1024);
+
+        $filename = 'images/'.Str::random(40).'.'.$file->extension();
+
+        Storage::disk('public')->put($filename, (string) $image->encodeByExtension($file->extension()));
 
         return back()->with('success', 'You have uploaded an image succesfully!');
     }
