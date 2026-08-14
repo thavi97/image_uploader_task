55abc827dd4c25d3e2760364340b296ab2ebb8a7
Thavi <thavit97@gmail.com>
2026-08-14 11:52:26 +0100

    create the image upload form

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,6 +2,15 @@
     <section>
         <h1>Image Uploader</h1>
 
-        <p>testing</p>
+        <form method="POST" action="#" enctype="multipart/form-data">
+            @csrf
+
+            <div>
+                <label for="image">Choose an image</label>
+                <input type="file" id="image" name="image" accept="image/*">
+            </div>
+
+            <button type="submit">Upload</button>
+        </form>
     </section>
 </x-app-layout>
