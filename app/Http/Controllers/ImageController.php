d09b565bb564fb6bc774235eb0d0faaa2900b173
Thavi <thavit97@gmail.com>
2026-08-14 11:57:56 +0100

    Store function for local storage

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'image' => [
+                'required',
+                'image',
+                'mimes:jpg,jpeg,png',
+                'max:2048', // 2MB
+            ],
+        ]);
+
+        $path = $request->file('image')->store('images', 'public');
+
+        return back()->with('success', 'You have uploaded an image succesfully!');
+    }
 }
